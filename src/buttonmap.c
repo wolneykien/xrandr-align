14f32f4cb911a0086ccec94a64eb61533dc332bf
Peter Hutterer <peter@cs.unisa.edu.au>
2007-12-03 12:40:03 +1030

    Remove trailing whitespaces (buttonmap.c)

--- src/buttonmap.c
+++ src/buttonmap.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 1996 by Frederic Lepied, France. <Frederic.Lepied@sugix.frmug.org>
- *                                                                            
+ *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is  hereby granted without fee, provided that
  * the  above copyright   notice appear  in   all  copies and  that both  that
@@ -9,8 +9,8 @@
  * advertising or publicity pertaining to distribution of the software without
  * specific,  written      prior  permission.     Frederic  Lepied   makes  no
  * representations about the suitability of this software for any purpose.  It
- * is provided "as is" without express or implied warranty.                   
- *                                                                            
+ * is provided "as is" without express or implied warranty.
+ *
  * FREDERIC  LEPIED DISCLAIMS ALL   WARRANTIES WITH REGARD  TO  THIS SOFTWARE,
  * INCLUDING ALL IMPLIED   WARRANTIES OF MERCHANTABILITY  AND   FITNESS, IN NO
  * EVENT  SHALL FREDERIC  LEPIED BE   LIABLE   FOR ANY  SPECIAL, INDIRECT   OR
@@ -40,17 +40,17 @@ set_button_map(Display	*display,
 	fprintf(stderr, "usage: xinput %s %s\n", name, desc);
 	return EXIT_FAILURE;
     }
-    
+
     info = find_device_info(display, argv[0], True);
-    
+
     if (!info) {
 	fprintf(stderr, "unable to find device %s\n", argv[0]);
 	return EXIT_FAILURE;
-    }    
-    
+    }
+
     ip = (XAnyClassPtr) info->inputclassinfo;
     nbuttons = 0;
-	    
+
     /* try to find the number of buttons */
     for(i=0; i<info->num_classes; i++) {
 	if (ip->class == ButtonClass) {
@@ -63,19 +63,19 @@ set_button_map(Display	*display,
 	fprintf(stderr, "device has no buttons\n");
 	return EXIT_FAILURE;
     }
-	    
+
     device = XOpenDevice(display, info->id);
     if (device) {
 	int		idx;
 	unsigned char	*map;
 	int		min;
-		
+
 	map = (unsigned char *) malloc(sizeof(unsigned char) * nbuttons);
-		
+
 	XGetDeviceButtonMapping(display, device, map, nbuttons);
-		
+
 	min = (argc > nbuttons + 1) ? nbuttons + 1 : argc;
-		
+
 	for(idx=1; idx < min; idx++) {
 	    map[idx - 1] = atoi(argv[idx]);
 	}
